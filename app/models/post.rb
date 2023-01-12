a2b6f80c286f6e4631133006b245e36287414a34
frank <franksavena@gmail.com>
2023-01-12 09:47:06 -0600

    commit tree 10959fb29c8bee731cb43ab6d216f5a7c7cb977e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction) }
+    validate :clickbate 
+
+    def clickbate
+        if title ==! `Won't Believe` || "Secret" || "Top [number]" || "Guess"
+            errors.add(:title, " please add clickbate ")
+        end
+    end
 end
